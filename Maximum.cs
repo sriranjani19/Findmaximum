e7be18e57786646f1afe60f91fae822a600a1e7f
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-03 21:23:35 +0530

    Add files via upload

---
+++ Maximum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumGenerics
+{
+   public class Maximum
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Number USing Generics\n");
+
+            //IntegerMax integerMax = new IntegerMax();
+            //integerMax.MaximumInteger(2, 6, 4);
+
+            //FloatMax floatMax = new FloatMax();
+            //floatMax.MaximumFloatNum(6.8,3.9,18.9);
+
+            //StringMax stringMax = new StringMax();
+            //stringMax.MaximumString("Apple", "Peach", "Zebra");
+
+
+
+
+        }
+    }
+} 
+
+    
+    
+    
+
